b085641ace592f78b1570a634f23b101fd97b7c3
Anto A <antraniga@gmail.com>
2016-10-10 09:41:18 -0700

    1

---
+++ DrawingProject.js
@@ -0,0 +1,45 @@
+var canvasDiv = document.getElementById("drawingArea");
+var canvas = canvasDiv.getContext('2d');
+
+canvas.fillStyle = "black";
+
+canvas.fillRect(0, 0, 1600, 900);
+
+canvas.beginPath();
+canvas.fillStyle = "#264d00";
+canvas.moveTo(532,680);
+canvas.lineTo(790,25);
+canvas.lineTo(1054,680);
+canvas.fill();
+
+canvas.beginPath();
+canvas.fillStyle = "#428000";
+canvas.moveTo(580,616);
+canvas.lineTo(790,83);
+canvas.lineTo(1000,616);
+canvas.fill();
+
+canvas.beginPath();
+canvas.fillStyle = "#264d00";
+canvas.moveTo(580,616);
+canvas.lineTo(790,514);
+canvas.lineTo(1000,616);
+canvas.fill();
+
+canvas.beginPath();
+canvas.fillStyle = "#264d00";
+canvas.fillRect(790,500,37,50);
+
+canvas.beginPath();
+canvas.fillStyle = "#264d00";
+canvas.moveTo(790,500);
+canvas.lineTo(790,190);
+canvas.lineTo(910,510);
+canvas.fill();
+
+canvas.beginPath();
+canvas.fillStyle = "black";
+canvas.moveTo(532,680);
+canvas.lineTo(790,546);
+canvas.lineTo(1054,680);
+canvas.fill();
