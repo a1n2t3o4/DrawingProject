554a83a7a06c93f818418254ea6ec692280d5a0d
Anto A <antraniga@gmail.com>
2016-10-10 22:30:38 -0700

    2

--- DrawingProject.js
+++ DrawingProject.js
@@ -6,7 +6,7 @@ canvas.fillStyle = "black";
 canvas.fillRect(0, 0, 1600, 900);
 
 canvas.beginPath();
-canvas.fillStyle = "#264d00";
+canvas.fillStyle = "#0C4C05";
 canvas.moveTo(532,680);
 canvas.lineTo(790,25);
 canvas.lineTo(1054,680);
@@ -20,18 +20,25 @@ canvas.lineTo(1000,616);
 canvas.fill();
 
 canvas.beginPath();
-canvas.fillStyle = "#264d00";
+canvas.fillStyle = "#0C4C05";
 canvas.moveTo(580,616);
 canvas.lineTo(790,514);
 canvas.lineTo(1000,616);
 canvas.fill();
 
 canvas.beginPath();
-canvas.fillStyle = "#264d00";
+canvas.fillStyle = "#0C4C05";
 canvas.fillRect(790,500,37,50);
 
 canvas.beginPath();
-canvas.fillStyle = "#264d00";
+canvas.fillStyle = "#0C4C05";
+canvas.moveTo(790,500);
+canvas.lineTo(790,190);
+canvas.lineTo(910,510);
+canvas.fill();
+
+canvas.beginPath();
+canvas.fillStyle = "#0C4C05";
 canvas.moveTo(790,500);
 canvas.lineTo(790,190);
 canvas.lineTo(910,510);
